f87524266ffd346500444f0e7fc9ccb9598d3f4f
lenovo <yosepalexsander9@gmail.com>
2021-07-05 15:04:04 +0700

    done css modules template

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,46 @@
-// create style here
+// import css modules
 
-// create Form component
+const styles = {
+  form: {
+    margin: "16px 20% 0",
+  },
+  formGroup: {
+    marginBottom: "16px",
+  },
+  formLabel: {
+    marginBottom: "8px",
+    display: "inline-block",
+  },
+  formInput: {
+    display: "block",
+    width: "100%",
+    padding: ".375rem .75rem",
+    fontSize: "1rem",
+    lineHeight: 1.5,
+    color: "#212529",
+    backgroundColor: "#fff",
+    border: "1px solid #ced4da",
+    borderRadius: ".25rem",
+  },
+};
+
+
 function Form() {
   return (
-    // code inside div
     <div>
-      {/* form */}
+      <form style={styles.form}>
+        <div style={styles.formGroup}>
+          <label htmlFor="username" style={styles.formLabel}>
+            Username
+          </label>
+          <input
+            id="username"
+            placeholder="Input username"
+            style={styles.formInput}
+          />
+        </div>
+        {/* code here */}
+      </form>
     </div>
   )
 }
