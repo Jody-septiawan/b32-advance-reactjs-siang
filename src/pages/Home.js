12438e60b4c8d17ad530e618ff12738e533f2713
Jody-septiawan <jody.septiawan5@gmail.com>
2022-03-29 14:51:13 +0700

    useNavigate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,26 @@
-// import necessary react-bootstrap component 
-import { Container } from "react-bootstrap";
+// import necessary react-bootstrap component
+import { Container, Button } from 'react-bootstrap';
 
 // import hooks from react-router-dom here
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
-
   // call that hooks here and store to variable
+  const navigate = useNavigate();
 
   // create function here for handle push to another pages
+  const handleNavigatetoSignIn = () => {
+    // ambil
+    // simpan
+
+    const kondisi = true;
+
+    if (kondisi == false) {
+      navigate('/signin');
+    } else {
+      alert('DILARANG PINDAH HALAMAN!');
+    }
+  };
 
   return (
     <Container className="text-center">
@@ -22,7 +35,7 @@ function Home() {
         parturient montes, nascetur ridiculus mus. Donec et maximus tellus, sit
         amet hendrerit augue.
       </p>
-      {/* code element here */}
+      <Button onClick={handleNavigatetoSignIn}>Click to SigIn</Button>
     </Container>
   );
 }
